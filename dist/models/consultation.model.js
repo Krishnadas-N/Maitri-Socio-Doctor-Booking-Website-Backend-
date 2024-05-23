3879999731a404b668a89217b7e03d66ffb532ce
Krishnadas N <mkkrishnadas25@gmail.com>
2024-05-23 11:15:28 +0530

    Remove dist directory from version control

--- dist/models/consultation.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// this.appointmentForm = this.fb.group({
-//     typeofConsultaion: [this.appoinMentDetail.typeofConsultaion, Validators.required],
-//     bookingDate: [this.appoinMentDetail.bookingDate, Validators.required],
-//     slotTime: [this.appoinMentDetail.slotTime, Validators.required]
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allowedStatuses = void 0;
-exports.allowedStatuses = ['Pending', 'Scheduled', 'Completed', 'Cancelled', 'Rejected'];
